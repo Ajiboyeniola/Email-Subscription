fb0166a58541fa0e8334152bcb3b0841327059a7
ajiboyeniola <ajiboyeniola@gmail.com>
2023-11-02 12:54:10 +0100

    Initial commit

---
+++ My folder/dist/script.js
@@ -0,0 +1,7 @@
+// let btn = document.getElementById("btn");
+
+// form.onclick = function subscribe("submit, e") {
+//   let school = document.createElement("h1");
+//   school.innerText = email.ariaValueMax;
+//   console.log("school");
+// };
---
+++ My folder/js/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("formFill");
+
+form.addEventListener("submit", function () {
+  let email = document.getElementById("email").value;
+  alert(email);
+});
